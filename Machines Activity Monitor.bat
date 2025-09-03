67132520143b9dd04363eb0fb2b882bbb6702200
albertodengobenozzi <alberto.dengo@benozzi.com>
2025-09-03 09:07:29 +0200

    Create Machines Activity Monitor.bat
    Creato l'eseguibile per lanciare l'app

---
+++ Machines Activity Monitor.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Vai nella cartella del progetto
+cd C:\Users\ALBDEN\Documents\GitHub\activity-monitor-19-integrex-j200
+
+REM Chiudi eventuali processi Streamlit rimasti appesi sulla porta 8501
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :8501') do taskkill /F /PID %%a >nul 2>&1
+
+REM Avvia l'app Streamlit
+python -m streamlit run streamlit_app.py --server.address 192.168.50.34 --server.port 8501
